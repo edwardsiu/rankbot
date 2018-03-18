325ca226575d31c0e74d960df57d6f5e53937381
esiu <siu.edward.w@gmail.com>
2018-03-17 20:18:23 -0700

    make game ids lowercase

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 def generate_game_id(hasher, msg_id, matches):
     msg_id = int(msg_id)
     while(True):
-        game_id = hasher.encode(msg_id)[:4]
+        game_id = hasher.encode(msg_id)[:4].lower()
         if not matches.find_one({"game_id": game_id}):
             return game_id
         msg_id -= 1
