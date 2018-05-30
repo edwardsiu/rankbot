b8495152e602ab1b1b6859d8a16973609bca2b58
esiu <siu.edward.w@gmail.com>
2018-05-29 21:57:32 -0700

    fix scale factor

--- src/database.py
+++ src/database.py
@@ -244,7 +244,7 @@ class RankDB(MongoClient):
         for player in losers:
             avg_opponent_score = (sum([i["points"] for i in losers if i != player]) + winner["points"])/3.0
             score_diff = player["points"] - avg_opponent_score
-            loss = int(round(6.0/(1+1.025**(-score_diff)) + 7))
+            loss = int(round(12.0/(1+1.0065**(-score_diff)) + 4))
             gains += loss
             members.update_one(
                 {"user_id": player["user_id"]},
