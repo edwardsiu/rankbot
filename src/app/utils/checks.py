b5998975a760a47650d9d26e7f723f73ceea9d1c
esiu <siu.edward.w@gmail.com>
2020-11-21 13:35:03 -0800

    update discord version and check

--- src/app/utils/checks.py
+++ src/app/utils/checks.py
@@ -10,7 +10,7 @@ async def is_registered(ctx):
 async def is_admin(ctx):
     if await ctx.bot.is_owner(ctx.message.author):
         return True
-    if ctx.message.author.id == ctx.message.guild.owner.id:
+    if ctx.message.author.id == ctx.message.guild.owner_id:
         return True
     admin_role = ctx.bot.db.get_admin_role(ctx.message.guild)
     if not discord.utils.find(lambda r: r.name == admin_role.name, ctx.message.author.roles):
